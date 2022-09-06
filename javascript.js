24c00a43f331e0b6020baaf828b50b486ae66581
Marcelo Garza <109047450+Linkgza@users.noreply.github.com>
2022-09-06 12:35:44 -0500

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <title> My first proper HTML page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <p>My rist web page</p>
+        <script>
+            const message = "Hello World!";
+            console.log(message);
+        </script>
+    </body>
+</html>
